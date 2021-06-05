ba4c124518062c566b325b9ce583c66f342c07fd
Erin Villasenor <villaser@oregonstate.edu>
2021-06-05 15:08:21 -0700

    Removed unnecessary code

--- app.js
+++ app.js
@@ -6,30 +6,12 @@ const chk = document.getElementById('chk');
 
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark-theme');
-    //localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark-theme' : 'light-theme')
     localStorage.setItem('toggleState', "true");
 });
-
-/*switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
-    localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark-theme' : 'light-theme')
-
-    var className = document.body.className;
-    if (className == "light-theme") {
-        this.textContent = "Dark Mode";
-    } else {
-        this.textContent = "Light Mode";
-    }
-
-    console.log('current class name: ' + className);
-});*/
 
 if (localStorage.getItem('theme') === 'dark-theme') {
     document.body.classList.add('dark-theme');
     switcher.textContent = "Light Mode";
-    // document.body.classList.toggle('dark-theme');
-
-
 }
 
 var tState = localStorage.getItem('toggleState');
