467c26ac8cc6491589c5dc40a19a604fc4f0bf73
Erin Villasenor <villaser@oregonstate.edu>
2021-06-04 23:53:32 -0700

    second commit

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 'use strict'
 
-const switcher = document.querySelector('.btn');
+const switcher = document.querySelector('.btnToDark');
 
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
     localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark-theme' : 'light-theme')
 
     var className = document.body.className;
-    var icon = document.getElementById('icon');
     if (className == "light-theme") {
         this.textContent = "Dark Mode";
-        //   this.icon.classList = ('fas fa-moon');
-        this.icon.classList.toggle('fas fa-moon');
-        this.icon.classList.toggle('fas fa-sun');
-
     } else {
         this.textContent = "Light Mode";
-        //   this.icon.classList = ('fas fa-sun');
-        this.icon.classList.toggle('fas fa-sun');
-        this.icon.classList.toggle('fas fa-moon');
-
-
     }
 
     console.log('current class name: ' + className);
@@ -29,7 +19,5 @@ switcher.addEventListener('click', function() {
 if (localStorage.getItem('theme') === 'dark-theme') {
     document.body.classList.add('dark-theme');
     switcher.textContent = "Light Mode";
-    switcher.icon.classList.toggle('fas fa-sun');
-    switcher.icon.classList.toggle('fas fa-moon');
 
 }
