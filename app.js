53ac5b383ccf09478d858763affbad004142c992
Erin Villasenor <villaser@oregonstate.edu>
2021-06-24 11:56:11 -0700

    Removed splide, fixed nightmode bug

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const chk = document.getElementById('chk');
-import Splide from '@splidejs/splide';
 
-new Splide('.splide').mount();
 
 if (localStorage.getItem('theme')) {
     document.body.classList.toggle('dark-theme');
@@ -15,21 +13,4 @@ chk.addEventListener('change', () => {
     } else {
         localStorage.setItem('theme', "dark-theme");
     }
-});
-
-var elms = document.getElementsByClassName('splide');
-for (var i = 0, len = elms.length; i < len; i++) {
-    new Splide(elms[i]).mount();
-}
-
-new Splide('#splide', {
-    type: 'fade',
-    rewind: true,
-}).mount();
-
-document.addEventListener('DOMContentLoaded', function() {
-    new Splide('#image-slider', {
-        'cover': true,
-        'heightRatio': 0.5,
-    }).mount();
 });
